d1ca57c65fde61cad309e5fe4da6b6d5710fd74a
Chris Joakim <christopher.joakim@gmail.com>
2018-06-13 14:05:33 -0400

    Implemented /blob_io.py - listing, upload, download

--- examples/blob_io.py
+++ examples/blob_io.py
@@ -10,15 +10,16 @@ import time
 from azure.storage.blob import BlockBlobService
 
 # python blob_io.py --func list_blobs --cname batchcsv
-# python blob_io.py --func upload_state_csv
-# python blob_io.py --func download --patterns <comma-separated-patterns>
-# python blob_io.py --func download --patterns log-info-states,results-info-states
+# python blob_io.py --func upload_state_csv --cname batchcsv
+# python blob_io.py --func download --cname batchcsv --patterns <comma-separated-patterns>
+# python blob_io.py --func download --cname batchcsv --patterns postal_codes,log-info-states,results-info-states
 
 
 def create_blob_service():
     account_name = os.environ['AZURE_STORAGE_ACCOUNT']
     account_key  = os.environ['AZURE_STORAGE_KEY']
     return BlockBlobService(account_name=account_name, account_key=account_key)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -39,10 +40,22 @@ if __name__ == '__main__':
     elif args.func == 'upload_state_csv':
         states = 'CT,FL,GA,MD,NC,SC,VA'.lower().split(',')
         for st in states:
-            print(st)
-
+            basename = 'postal_codes_{}.csv'.format(st)
+            infile = 'data/{}'.format(basename)
+            blob_svc.create_blob_from_path(args.cname, basename, infile)
+            print('uploaded {} to {}'.format(infile, args.cname))
 
     elif args.func == 'download':
-        patterns = args.patterns.split(',')
-        for pattern in patterns:
-            print(pattern)
+        print('downloading blobs from container {} ...'.format(args.cname))
+        patterns  = args.patterns.split(',')
+        generator = blob_svc.list_blobs(args.cname)
+        blob_list = list()
+        for blob in generator:
+            matched = False
+            for pattern in patterns:
+                if pattern in blob.name:
+                    matched = True
+            if matched:
+                outfile = 'tmp/{}'.format(blob.name)
+                blob_svc.get_blob_to_path(args.cname, blob.name, outfile)
+                print('downloaded {}'.format(blob.name))
