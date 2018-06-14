8550eee7a8a300acda617020f3b427d25acb09a8
Chris Joakim <christopher.joakim@gmail.com>
2018-06-14 10:11:22 -0400

    Dockerfile for statecenter.py with st env var

--- examples/statecenter.py
+++ examples/statecenter.py
@@ -5,10 +5,11 @@ import sys
 import pandas as pd
 
 # Get the mean latitude/longitude location for the given US state code.
-# Chris Joakim, Microsoft, 2018/06/12
-# python states_local.py state_center ct
+# Chris Joakim, Microsoft, 2018/06/14
+# export st=fl ; python statecenter.py state_center
 
 def describe(df):
+    print("describe the pandas DataFrame")
     print('=== type(df)')
     print(type(df))  # <class 'pandas.core.frame.DataFrame'>
     print('=== df.columns')
@@ -23,6 +24,8 @@ def describe(df):
     print(df.dtypes)
     print('=== df.describe()')
     print(df.describe())
+    print("")
+    print("")
 
 if __name__ == "__main__":
 
@@ -30,7 +33,7 @@ if __name__ == "__main__":
         func = sys.argv[1].lower()
 
         if func == 'state_center':
-            st = func = sys.argv[2].lower()
+            st = os.environ["st"].lower()
             infile = 'data/postal_codes_{}.csv'.format(st)
             print('st, infile: {} -> {}'.format(st, infile))
             df = pd.read_csv(infile, delimiter=',')
@@ -40,6 +43,7 @@ if __name__ == "__main__":
             mean_lng = df["longitude"].mean()
             csv_line = '{},{},{}'.format(st, mean_lat, mean_lng)
             print(csv_line)
+            print("")
         else:
             print("invalid functio: {}".format(func))
     else:
