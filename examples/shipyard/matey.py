5d6dec504ce4aee1dc3617ea8a46414ee5b98163
Chris Joakim <christopher.joakim@gmail.com>
2018-07-30 12:10:11 -0400

    Updated matey.py

--- examples/shipyard/matey.py
+++ examples/shipyard/matey.py
@@ -1,12 +1,13 @@
 """
 Usage:
   python matey.py --function gen_job_env_vars
+  python matey.py --function parse_config_files
 Options:
   -h --help     Show this screen.
   --version     Show version.
 """
 
-# Chris Joakim, Microsoft, 2018/07/27
+# Chris Joakim, Microsoft, 2018/07/30
 
 from __future__ import print_function
 import argparse
@@ -27,6 +28,22 @@ def gen_job_env_vars():
             v = os.getenv(n)
             print('    {}: {}'.format(n, v))
 
+def parse_config_files():
+    print('parse_config_files')
+
+    config_basenames = 'config credentials jobs pool'.split()
+    print(config_basenames)  # ['config', 'credentials', 'jobs', 'pool']
+
+    for basename in config_basenames:
+        infile = 'config/{}.yaml'.format(basename)
+        print('reading/parsing file: {}'.format(infile))
+        with open(infile, 'r') as stream:
+            try:
+                obj = yaml.load(stream)
+                print(json.dumps(obj, sort_keys=True, indent=2))
+            except yaml.YAMLError as exc:
+                print(exc)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -35,6 +52,9 @@ if __name__ == '__main__':
 
     if args.function == 'gen_job_env_vars':
         gen_job_env_vars()
+
+    elif args.function == 'parse_config_files':
+        parse_config_files()
 
     else:
         print('unknown function: {}'.format(args.function))
