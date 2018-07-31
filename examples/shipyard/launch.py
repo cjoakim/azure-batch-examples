d52568b90860d940b7bf3a656cebd843e0a1dae3
Chris Joakim <christopher.joakim@gmail.com>
2018-07-31 17:27:17 -0400

    launch.py generating config files per launch.json and templates in config_examples

--- examples/shipyard/launch.py
+++ examples/shipyard/launch.py
@@ -1,17 +1,21 @@
 """
 Usage:
   python launch.py --function gen_config_files
-  python launch.py --function gen_job_env_vars
   python launch.py --function parse_config_files
 Options:
   -h --help     Show this screen.
   --version     Show version.
 """
 
+# The purpose of this script is to both generate and parse/validate
+# Shipyard configuration files.  It is a working proof-of-concept
+# for a more complete implementation.  For config file generation,
+# edit the 'launch.json' config file with your specific values.
 # Chris Joakim, Microsoft, 2018/07/31
 
 from __future__ import print_function
 import argparse
+import arrow
 import collections
 import csv
 import datetime
@@ -23,14 +27,66 @@ import time
 import yaml
 
 
-def launch_config():
-    # TODO - read this dict from a json file
-    c = dict()
-    c['docker_image'] = 'cjoakim/azure-batch-shipyard1:latest'
-    c['storage_acct'] = 'cjoakimstdstorage'
-    c['storage_key']  = find_env_var_key(c['storage_acct'])
+def gen_config_files():
+    print('gen_config_files')
+    c = launch_config()
+    gen_conf_config_yaml(c)
+    gen_credentials_config_yaml(c)
+    gen_jobs_config_yaml(c)
+    gen_pool_config_yaml(c)
+    print('Shipyard config files generated per the following values:')
     print(json.dumps(c, sort_keys=True, indent=2))
+
+def gen_conf_config_yaml(c):
+    template_obj = load_yaml_file('config_examples/config.yaml')
+    template_obj['batch_shipyard']['storage_account_settings'] = c['storage_acct']
+    template_obj['global_resources']['docker_images'][0] = c['docker_image']
+    write_yaml(template_obj, 'config')
+
+def gen_credentials_config_yaml(c):
+    template_obj = load_yaml_file('config_examples/credentials.yaml')
+    template_obj['credentials']['batch']['account_key'] = os.getenv('AZURE_BATCH_KEY')
+    template_obj['credentials']['batch']['account_service_url'] = os.getenv('AZURE_BATCH_URL')
+    stor_dict, acct_dict, stor_acct = dict(), dict(), c['storage_acct']
+    acct_dict['account'] = stor_acct
+    acct_dict['account_key'] = c['storage_key']
+    acct_dict['endpoint'] = 'core.windows.net'
+    stor_dict[stor_acct] = acct_dict
+    template_obj['credentials']['storage'] = stor_dict
+    write_yaml(template_obj, 'credentials')
+
+def gen_jobs_config_yaml(c):
+    jobname = '{}{}'.format(c['job_basename'], c['epoch'])
+    template_obj = load_yaml_file('config_examples/jobs.yaml')
+    template_obj['job_specifications'][0]['id'] = jobname
+    env_vars = dict()
+    for env_var in c['job_env_var_names']:
+        env_vars[env_var] = os.getenv(env_var)
+    template_obj['job_specifications'][0]['environment_variables'] = env_vars
+    template_obj['job_specifications'][0]['tasks'][0]['docker_image'] = c['docker_image']
+    template_obj['job_specifications'][0]['tasks'][0]['command'] = c['command']
+    write_yaml(template_obj, 'config')
+    c['job_env_var_values'] = env_vars
+
+def gen_pool_config_yaml(c):
+    template_obj = load_yaml_file('config_examples/pool.yaml')
+    template_obj['pool_specification']['id'] = c['pool_id']
+    template_obj['pool_specification']['vm_size'] = c['vm_size']
+    template_obj['pool_specification']['vm_count']['dedicated'] = c['vm_count']
+    write_yaml(template_obj, 'pool')
+
+def launch_config():
+    c = read_json_file('launch.json')
+    c['epoch'] = arrow.utcnow().timestamp
+    c['storage_key']  = find_env_var_key(c['storage_acct'])
+    c['pool_id'] = pool_id(c)
     return c
+
+def pool_id(c):
+    if 'pool_id' in c:
+        return c['pool_id']
+    else:
+        return 'cjoakimpool{}'.format(c['epoch'])
 
 def find_env_var_key(name_value):
     # by convention, environment variables in this pattern are expected:
@@ -45,94 +101,14 @@ def find_env_var_key(name_value):
                 return os.getenv(key)
     return '?'
 
-def gen_config_files():
-    print('gen_config_files')
-    c = launch_config()
-    gen_conf_config_yaml(c)
-    gen_credentials_config_yaml(c)
-    gen_jobs_config_yaml(c)
-    gen_pool_config_yaml(c)
-
-def gen_conf_config_yaml(c):
-    data = dict()
-    batch_dict = dict()
-    batch_dict['storage_account_settings'] = c['storage_acct']
-    data['batch_shipyard'] = batch_dict
-    globals = dict()
-    globals['docker_images'] = [c['docker_image']]
-    data['global_resources'] = globals
-    write_yaml(data, 'config')
-
-def gen_credentials_config_yaml(c):
-    data = dict()
-    cred_dict = dict()
-    batch_dict = dict()
-    batch_dict['account_key'] = os.getenv('AZURE_BATCH_KEY')
-    batch_dict['account_service_url'] = os.getenv('AZURE_BATCH_URL')
-    stor_dict, acct_dict, stor_acct = dict(), dict(), c['storage_acct']
-    acct_dict['account'] = stor_acct
-    acct_dict['account_key'] = c['storage_key']
-    acct_dict['endpoint'] = 'core.windows.net'
-    stor_dict[stor_acct] = acct_dict
-    cred_dict['batch'] = batch_dict
-    cred_dict['storage'] = stor_dict
-    data['credentials'] = cred_dict
-    write_yaml(data, 'credentials')
-
-    # credentials:
-    #   batch:
-    #     account_key: <secret>
-    #     account_service_url: https://cjoakimbatch.eastus.batch.azure.com
-    #   storage:
-    #     cjoakimstdstorage:
-    #       account: cjoakimstdstorage
-    #       account_key: <secret>
-    #       endpoint: core.windows.net
-
-def gen_jobs_config_yaml(c):
-    data = dict()
-    write_yaml(data, 'jobs')
-
-    # job_specifications:
-    # - id: cjoakimshipyard4
-    #   environment_variables:
-    #     AZURE_STORAGE_ACCOUNT: cjoakimstdstorage
-    #     AZURE_STORAGE_KEY: 7SPUdEMEMV185ZY/JDlCvybizujNLGoGqR9U5sKYcO3RTS60lo9lEvXdgk7U0DeILYlTQdk/6bBHsT6WM5E4Fw==
-    #   tasks:
-    #   - docker_image: cjoakim/azure-batch-shipyard1:latest
-    #     remove_container_after_exit: true
-    #     command: env | sort ; docker run -d -e AZURE_STORAGE_ACCOUNT=$AZURE_STORAGE_ACCOUNT -e AZURE_STORAGE_KEY=$AZURE_STORAGE_KEY cjoakim/azure-batch-shipyard1:latest
-
-def gen_pool_config_yaml(c):
-    data = dict()
-    write_yaml(data, 'pool')
-
-    # pool_specification:
-    #   id: cjoakimpool30
-    #   vm_size: Standard_A5
-    #   vm_count:
-    #     dedicated: 1
-    #     low_priority: 0
-    #   vm_configuration:
-    #     platform_image:
-    #       publisher: Canonical
-    #       offer: UbuntuServer
-    #       sku: 16.04-LTS
-    #   reboot_on_start_task_failed: false
-    #   block_until_all_global_resources_loaded: true
-
 def write_yaml(obj, basename):
     outfile = 'config/{}_gen.yaml'.format(basename)
     with open(outfile, 'w') as out:
         yaml.dump(obj, out, default_flow_style=False)
         print('file written: {}'.format(outfile))
 
-def gen_job_env_vars():
-    print('  environment_variables:')
-    for n in sorted(os.environ):
-        if n.startswith('AZURE'):
-            v = os.getenv(n)
-            print('    {}: {}'.format(n, v))
+def read_json_file(infile):
+    return json.load(open(infile))
 
 def parse_config_files():
     print('parse_config_files')
@@ -151,16 +127,21 @@ def parse_config_files():
             except yaml.YAMLError as exc:
                 print(exc)
 
+def load_yaml_file(infile):
+    with open(infile, 'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--function', required=True, help='The logical function to execute')
     args = parser.parse_args()
 
-    if args.function == 'gen_job_env_vars':
-        gen_job_env_vars()
-
-    elif args.function == 'gen_config_files':
+    if args.function == 'gen_config_files':
         gen_config_files()
 
     elif args.function == 'parse_config_files':
@@ -168,4 +149,3 @@ if __name__ == '__main__':
 
     else:
         print('unknown function: {}'.format(args.function))
-
